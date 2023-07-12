1583ef5eb3ed1c9d965e1df24171d8b17686cb2a
Wolfram-180 <sergei.ulvis@gmail.com>
2023-07-12 16:51:02 +0300

    upd

---
+++ lib/dio_demo.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+
+final dio = Dio(baseOptions);
+
+BaseOptions baseOptions = BaseOptions(
+  baseUrl: 'https://jsonplaceholder.typicode.com',
+  connectTimeout: const Duration(seconds: 10),
+  receiveTimeout: const Duration(seconds: 10),
+  sendTimeout: const Duration(seconds: 10),
+  responseType: ResponseType.json,
+  headers: {
+    'device-info': 'Nokia 3310 OS 14',
+  },
+);
+
+Future<dynamic> getPosts() async {
+  initInterceptors();
+  final postResponse = await dio.get(
+    '/posts',
+/*     queryParameters: {
+      'id': 1,
+    }, */
+  );
+
+  if (postResponse.statusCode == 200) {
+    return jsonDecode(postResponse.data);
+  }
+
+  throw Exception(
+    'error code ${postResponse.statusCode}',
+  );
+}
+
+Future<dynamic> createPost() async {
+  initInterceptors();
+  final postResponse = await dio.post(
+    '/posts',
+    queryParameters: {
+      'title': 'title',
+      'body': 'body',
+      'userId': '1',
+    },
+  );
+
+  if (postResponse.statusCode == 201) {
+    return postResponse.data;
+  }
+
+  throw Exception(
+    'error code ${postResponse.statusCode}',
+  );
+}
+
+void initInterceptors() {
+  dio.interceptors.add(
+    InterceptorsWrapper(
+      onError: (e, handler) {},
+      onRequest: (options, handler) {
+        print('request sent');
+      },
+      onResponse: (e, handler) {
+        print('response received');
+      },
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'post.dart';
+import 'dio_demo.dart';
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -32,13 +35,117 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  void initState() {
+    testNetworkCall();
+    super.initState();
+  }
+
+  void testNetworkCall() async {
+    final response = await getPosts();
+    final postsList = parsePosts(response);
+
+    print('Response HTTP call = $response');
+    print('postList = $postsList');
+  }
+
+  List<Post> parsePosts(String rawJson) {
+    List postListJson = jsonDecode(rawJson);
+
+    return postListJson.map((postJson) => Post.fromJson(postJson)).toList();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(),
+      body: const Center(),
     );
   }
 }
+
+
+/* import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'post.dart';
+import 'dio_demo.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  static const _title = 'Surf Flutter Course: Dio (module 10)';
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    testNetworkCall();
+    super.initState();
+  }
+
+  void testNetworkCall() async {
+    final response = await getPosts();
+    final postsList = parsePosts(response);
+
+    print('Response HTTP call = $response');
+    print('postList = $postsList');
+  }
+
+  List<Post> parsePosts(String rawJson) {
+    List postListJson = jsonDecode(rawJson);
+
+    return postListJson.map((postJson) => Post.fromJson(postJson)).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: MyApp._title,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: MyApp._title),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const Center(),
+    );
+  }
+}
+
+
+
+
+
+
+
+ */
+
