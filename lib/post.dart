6f332ce2eb0e29a85741d7e6003f8860800694fc
Wolfram-180 <sergei.ulvis@gmail.com>
2023-07-12 15:43:56 +0300

    added Post

---
+++ lib/post.dart
@@ -0,0 +1,27 @@
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Post id: $id, title: $title';
+  }
+}
